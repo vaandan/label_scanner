c1e12ce77e35d9a5f3506bc6aa5dfb8d2c9db815
vaandan <47479319+vaandan@users.noreply.github.com>
2024-11-21 12:37:19 +0530

    Delete label_scanner directory

--- label_scanner/doctype/label_scan_settings/label_scan_settings.js
+++
@@ -1,7 +0,0 @@
-frappe.ui.form.on('Label Scan Settings', {
-    refresh: function(frm) {
-        frm.add_custom_button(__('Test Scanner'), function() {
-            label_scanner.capture_image();
-        });
-    }
-});
